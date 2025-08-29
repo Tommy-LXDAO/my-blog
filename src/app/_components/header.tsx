656d04efee4d103a2a4718828b775e17140eb61a
Tommy <tommyskypromax@gmail.com>
2025-08-30 07:59:15 +0800

    add images datas
    Signed-off-by: Tommy <tommyskypromax@gmail.com>

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,19 +1,24 @@
 'use client';
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Header() {
     const router = useRouter();
-    console.log(`header re-render`)
     return (
-        <header className="h-10 w-dvw border-solid border-black dark:border-white border-b box-border flex justify-around">
-        <div className="w-1/5 flex justify-center-safe items-center"><span className="">程序员小屋</span></div>
+        <header className="h-10 w-dvw border-solid border-black border-b box-border flex justify-around">
+        <div className="w-1/5 flex justify-center-safe items-center">
+            <div className="h-5">
+                <Image src='/Icon_laptop_code.png' height={10} width={24} alt="Icon"/>
+            </div>
+            <div className=""><span className="">GeekSphere</span></div>
+        </div>
         <nav className="flex w-1/5 items-center justify-evenly">
-          <div><button onClick={() => {router.push(`/`)}}>首页</button></div>
-          <div><button onClick={() => {router.push(`/classification`)}}>分类</button></div>
-          <div><button onClick={() => {router.push(`/article`)}}>文章</button></div>
-          <div><button onClick={() => {router.push(`/`)}}>社区</button></div>
-          <div><button onClick={() => {router.push(`/`)}}>友链</button></div>
-          <div><button onClick={() => {router.push(`/`)}}>联系方式</button></div>
+          <div><button onClick={() => {router.push(`/`)}}>Home</button></div>
+          <div><button onClick={() => {router.push(`/classification`)}}>Blog</button></div>
+          <div><button onClick={() => {router.push(`/article`)}}>Community</button></div>
+          {/* <div><button onClick={() => {router.push(`/`)}}>社区</button></div> */}
+          <div><button onClick={() => {router.push(`/`)}}>Friends</button></div>
+          {/* <div><button onClick={() => {router.push(`/`)}}>联系方式</button></div> */}
         </nav>
       </header>
     )
--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,5 +1,6 @@
 'use client';
-
+// TODO: 添加文章分类，添加分类列表，搜索框，登录注册系统，markdown编辑栏
+// TODO: 后端开发、数据库表设计
 export default function Home() {
     return (
         <>Article</>
