96b483096951c32ed11b0c75871397350c3808ad
Tommy <tommyskypromax@gmail.com>
2025-09-16 00:34:22 +0800

    feat: add markdown page
    Signed-off-by: Tommy <tommyskypromax@gmail.com>

--- src/app/article/[blogId]/page.tsx
+++ src/app/article/[blogId]/page.tsx
@@ -9,18 +9,22 @@ import { useApi } from '@/hooks/useApi';
 // 详情页，通过接口获取文章的markdown raw data，并转换为样式
 export default function BlogPage({ params }: { params: Promise<{ blogId: string }> }) {
 
-  const [datas, setDatas] = useState<{item_id: string, raw_data: string}>();
+  const [datas, setDatas] = useState<{id: string, title: string, rawData: string}>();
 
   const { fetchApi } = useApi();
 
   const { blogId } = React.use(params);  // 解 Promise
+
+  console.log(`###render BlogPage datas=${datas}`)
+
   useEffect(() => {
     async function fetchBlogRawDatas() {
       try {
-        // const res = await fetchApi(`/mock/article_${blogId}.json`); // 从 public 目录读取
+        // const res = await fetch(`/mock/article_1.json`); // 从 public 目录读取
         const res = await fetchApi(`/articles/detail/${blogId}`); // 从 public 目录读取
         if (!res.ok) throw new Error("获取文章数据失败");
         const data = await res.json();
+        console.log(`data=${data.rawData}`)
         setDatas(data);
       } catch (err) {
         console.error("获取文章失败:", err);
@@ -28,7 +32,7 @@ export default function BlogPage({ params }: { params: Promise<{ blogId: string 
     }
 
     fetchBlogRawDatas();
-  }, [])
+  }, [fetchApi, blogId])
 
   return (
     <div className='flex justify-center items-center'>
@@ -37,7 +41,7 @@ export default function BlogPage({ params }: { params: Promise<{ blogId: string 
         {/* Here you can fetch and display the blog content based on the blogId */}
         <div className="fade-in-700">
           <Markdown remarkPlugins={[remarkGfm]}>
-            {datas ? datas.raw_data : ""}
+            {datas ? datas.rawData : ""}
           </Markdown>
         </div>
       </div>
